93f79fba69a3430dc369510a74b79d1f4ab27ce4
Arjen Hunter <arjen.hunter@nspyre.nl>
2015-05-21 21:21:55 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inverting a matrix can be a CPU time consuming activity. 
+## Especially when it has to be performed repeatedly.
 
-## Write a short comment describing this function
+## The first function, makeVector creates a special 
+## "vector", which is really a list containing a 
+## function to:
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse
+## 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y){
+		x <<- y
+		m <<- NULL
+	}
+	Get <- function() x
+	Setmatrix <- function(solve) m <<- solve
+	Getmatrix <- function() m
+	list(set=set, get = get,
+		setmatrix = setmatrix,
+		getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## The second function computes the inverse of the special 
+## "matrix" returned by makeCacheMatrix above. If the
+## inverse has already been calculated (and the matrix
+## has not changed), then the cachesolve should 
+## retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getmatrix()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	matrix <- x$mget()
+	m <- solve(matrix, ...)
+	x$setmatrix(m)
+	m
 }
